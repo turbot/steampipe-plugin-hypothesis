67ac3479a42be2a2b42c367e010968e7dbeb268c
misraved <ved@turbot.com>
2023-03-20 13:04:05 +0530

    Recompile plugin with steampipe-plugin-sdk v5.3.0

--- hypothesis/table_hypothesis_search.go
+++ hypothesis/table_hypothesis_search.go
@@ -7,9 +7,9 @@ import (
 	"os"
 
 	hyp "github.com/judell/hypothesis-go"
-	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 )
 
 func tableHypothesisSearch(ctx context.Context) *plugin.Table {
@@ -40,7 +40,7 @@ func tableHypothesisSearch(ctx context.Context) *plugin.Table {
 }
 
 func listSearchResults(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	quals := d.KeyColumnQuals
+	quals := d.EqualsQuals
 	qs := quals["query"].GetStringValue()
 
 	plugin.Logger(ctx).Info("hypothesis.listSearchResults", "qs", qs)
@@ -101,7 +101,7 @@ func listSearchResults(ctx context.Context, d *plugin.QueryData, h *plugin.Hydra
 
 		d.StreamListItem(ctx, row)
 
-		if d.QueryStatus.RowsRemaining(ctx) == 0 {
+		if d.RowsRemaining(ctx) == 0 {
 			break
 		}
 
